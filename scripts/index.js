e229da8a3e4c2cef9258b7c3e5b8163d18ed24cb
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-07-20 16:44:26 +0300

    revesion 1

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// let fullname = "mohmmad alissa";
+// let age = 22 ; 
+// let student =false ; 
+
+// document.getElementById("p1").textContent =  `your name is ${fullname}` ; 
+// document.getElementById("p2").textContent = age ; 
+// document.getElementById("p3").textContent = student ; 
+//arithmetic operations 
+// let students = 30 
+// students %= 2
+
+// console.log(students)
+// let result = 2*1 + 2 + 4/12
+// console.log( result)
+
+
+
+// how to accept user input 
+//first way
+// let username  ; 
+// username= window.prompt("what is your username? ");
+// console.log(username)
+//second way
+// let username ; 
+// document.getElementById("mysubmit").onclick = function() {
+//     username = document.getElementById("My-text").value ; 
+//     document.getElementById("MyId").textContent = `hello ${username}`
+//     console.log(username)
+// }
+
+
+
